896b4d40e91509bb54535a1d305ba2b8cc816be9
Joseph Brown <84300225+Joseph-K-B@users.noreply.github.com>
2022-01-13 17:46:34 -0800

    FE/BE communications;

--- src/services/profile.js
+++ src/services/profile.js
@@ -3,8 +3,15 @@ import { client } from './createClient';
 const URL = process.env.REACT_APP_DATABASE_URL;
 
 export async function getProfile() {
-  const res = await fetch(`https://localhost:3000/api/v1/users`);
+  const url = `http://localhost:3000/api/v1/users`;
+  const res = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
   const data = await res.json();
+  console.log('RES', data);
   return data;
 }
 
