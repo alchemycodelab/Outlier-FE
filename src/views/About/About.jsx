edb225e49063ef6600d747a4d924d29bd263b2ff
maddenlockin <maddenlockin@gmail.com>
2022-01-17 15:12:36 -0800

    about component created

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -1,3 +1,10 @@
+import AboutComponent from "../../components/About/About";
+
 export default function About() {
-  return <h1> About Page </h1>;
+  return (
+    <>
+      <h1> About Page </h1>
+      <AboutComponent />
+    </>
+  );
 }
