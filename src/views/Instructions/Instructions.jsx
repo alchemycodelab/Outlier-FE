fe3e697307214261241a5f817c4b750e022d2a08
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2022-01-17 15:05:09 -0800

    updated instructions page

--- src/views/Instructions/Instructions.jsx
+++ src/views/Instructions/Instructions.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Header from '../Layout/Header';
 
 export default function Instructions() {
   return (
     <section>
-      <Header />
       <h1> Instructions Page </h1>
       <>
         <p>These are the instructions to use our 3d charts!</p>
