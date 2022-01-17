0d54e45cc4764020a3000dbff1d5aedefcc31cc5
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2022-01-17 14:59:15 -0800

    update instructions page

--- src/views/Instructions/Instructions.jsx
+++ src/views/Instructions/Instructions.jsx
@@ -1,3 +1,14 @@
+import React from 'react';
+import Header from '../Layout/Header';
+
 export default function Instructions() {
-  return <h1> Instructions Page </h1>;
+  return (
+    <section>
+      <Header />
+      <h1> Instructions Page </h1>
+      <>
+        <p>These are the instructions to use our 3d charts!</p>
+      </>
+    </section>
+  );
 }
