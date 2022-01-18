fb75095d7f1e154d1249a7eabbf6edabae3c67c7
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2022-01-18 14:07:29 -0800

    supabase uninstall

--- src/components/Forms/MapForm/MapForm.jsx
+++ src/components/Forms/MapForm/MapForm.jsx
@@ -1,13 +1,14 @@
-import { useEffect, useState } from "react";
-import { useActiveStates } from "../../../context/Profile/StateCtx";
-import { getDrinkingData } from "../../../services/data";
-import { getMissingData } from "../../../services/missingData";
-import { getPopsByState, getPopulations } from "../../../services/populations";
-import { getStates } from "../../../services/states";
+import { useEffect, useState } from 'react';
+import { useActiveStates } from '../../../context/Profile/StateCtx';
+import { getDrinkingData } from '../../../services/data';
+import { getMissingData } from '../../../services/missingData';
+import { getPopsByState, getPopulations } from '../../../services/populations';
+import { getStates } from '../../../services/states';
 
 export default function MapForm() {
-  const { stateNames, setStateNames, activeStates, setActiveStates } = useActiveStates();
-  const [loading, setLoading] = useState(true)
+  const { stateNames, setStateNames, activeStates, setActiveStates } =
+    useActiveStates();
+  const [loading, setLoading] = useState(true);
   const [stateSelection, setStateSelection] = useState([]);
   const [dataRes, setDataRes] = useState([]);
 
@@ -18,72 +19,73 @@ export default function MapForm() {
     };
     const fetchData = async () => {
       const res = await getPopsByState('AL');
-      console.log(res)
+      console.log(res);
       setDataRes(res);
-    }
+    };
     fetchData();
     fetchStates();
-    setLoading(false)
+    setLoading(false);
   }, []);
   // console.log(stateNames)
-  
+
   const handleStateSubmit = async (e) => {
     e.preventDefault();
     setActiveStates(stateSelection);
-    await getPopsByState()
-  }
+    await getPopsByState('AL');
+  };
 
-  return (
-    loading ? <h1>Loading..</h1> :
+  return loading ? (
+    <h1>Loading..</h1>
+  ) : (
     <>
-    {activeStates.map((stateName) => {
-      <h3>{stateName.stateName}</h3>
-    })}
-    <form onSubmit={handleStateSubmit}>
-      <select
-        value={stateNames.stateName}
-        onChange={(e) => stateSelection.push(e.target.value)}
-      >
-        {stateNames.map((stateName) => (
-          <option key={stateName.abrv}>{stateName.stateName}</option>
-        ))}
-      </select>
-      <select
-        value={stateNames.stateName}
-        onChange={(e) => stateSelection.push(e.target.value)}
-      >
-        {stateNames.map((stateName) => (
-          <option key={stateName.abrv}>{stateName.stateName}</option>
-        ))}
-      </select>
-      <select
-        value={stateNames.stateName}
-        onChange={(e) => stateSelection.push(e.target.value)}
-      >
-        {stateNames.map((stateName) => (
-          <option key={stateName.abrv}>{stateName.stateName}</option>
-        ))}
-      </select>
-      <select
-        value={stateNames.stateName}
-        onChange={(e) => stateSelection.push(e.target.value)}
-      >
-        {stateNames.map((stateName) => (
-          <option key={stateName.abrv}>{stateName.stateName}</option>
-        ))}
-      </select>
-      <select
-        value={stateNames.stateName}
-        onChange={(e) => stateSelection.push(e.target.value)}
-      >
-        {stateNames.map((stateName) => (
-          <option key={stateName.abrv}>{stateName.stateName}</option>
-        ))}
-      </select>
-      <button type='submit'>Submit</button>
-      <button onClick={console.log(activeStates)}>Test 2</button>
-      <button onClick={console.log(dataRes)}>Test 3</button>
-    </form>
+      {activeStates.map((stateName) => {
+        <h3>{stateName.stateName}</h3>;
+      })}
+      <form onSubmit={handleStateSubmit}>
+        <select
+          value={stateNames.stateName}
+          onChange={(e) => stateSelection.push(e.target.value)}
+        >
+          {stateNames.map((stateName) => (
+            <option key={stateName.abrv}>{stateName.stateName}</option>
+          ))}
+        </select>
+        <select
+          value={stateNames.stateName}
+          onChange={(e) => stateSelection.push(e.target.value)}
+        >
+          {stateNames.map((stateName) => (
+            <option key={stateName.abrv}>{stateName.stateName}</option>
+          ))}
+        </select>
+        <select
+          value={stateNames.stateName}
+          onChange={(e) => stateSelection.push(e.target.value)}
+        >
+          {stateNames.map((stateName) => (
+            <option key={stateName.abrv}>{stateName.stateName}</option>
+          ))}
+        </select>
+        <select
+          value={stateNames.stateName}
+          onChange={(e) => stateSelection.push(e.target.value)}
+        >
+          {stateNames.map((stateName) => (
+            <option key={stateName.abrv}>{stateName.stateName}</option>
+          ))}
+        </select>
+        <select
+          value={stateNames.stateName}
+          onChange={(e) => stateSelection.push(e.target.value)}
+        >
+          {stateNames.map((stateName) => (
+            <option key={stateName.abrv}>{stateName.stateName}</option>
+          ))}
+        </select>
+        <button type="submit">Submit</button>
+        <button onClick={console.log(activeStates)}>Test 2</button>
+        <button onClick={console.log(dataRes)}>Test 3</button>
+      </form>
     </>
   );
-};
+}
