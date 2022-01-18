c1c86c77888b7291370fe6a3a80b1d5a2e5361fa
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2022-01-17 18:01:54 -0800

    3rd orb

--- src/components/Three/Charts/Orb.jsx
+++ src/components/Three/Charts/Orb.jsx
@@ -1,7 +1,7 @@
 export default function Orb() {
   return (
     <group>
-      <mesh position={[4, 0, 0]} visible castShadow>
+      <mesh position={[-5, 6, 6]} visible castShadow>
         <sphereGeometry attach="geometry" args={[1.5, 16, 16]} />
         <meshStandardMaterial
           attach="material"
@@ -16,7 +16,17 @@ export default function Orb() {
         <meshStandardMaterial
           attach="material"
           color="red"
-          wireframe={true}
+          wireframe={false}
+          roughness={0.5}
+          metalness={0.5}
+        />
+      </mesh>
+      <mesh position={[6, 5, 5]} visible castShadow>
+        <sphereGeometry attach="geometry" args={[1.5, 16, 16]} />
+        <meshStandardMaterial
+          attach="material"
+          color="green"
+          wireframe={false}
           roughness={0.5}
           metalness={0.5}
         />
