733c88b62d77540284acba166e1f2a4d9d1c7fa9
Joseph Brown <84300225+Joseph-K-B@users.noreply.github.com>
2022-01-17 22:47:48 -0800

    testing services for map form

--- src/components/Forms/MapForm/MapForm.jsx
+++ src/components/Forms/MapForm/MapForm.jsx
@@ -1,24 +1,30 @@
 import { useEffect, useState } from "react";
 import { useActiveStates } from "../../../context/Profile/StateCtx";
+import { getDrinkingData } from "../../../services/data";
+import { getMissingData } from "../../../services/missingData";
 import { getStates } from "../../../services/states";
-
-
-
 
 export default function MapForm() {
   const { stateNames, setStateNames, activeStates, setActiveStates } = useActiveStates();
   const [loading, setLoading] = useState(true)
   const [stateSelection, setStateSelection] = useState([]);
+  const [dataRes, setDataRes] = useState([]);
 
   useEffect(() => {
     const fetchStates = async () => {
       const res = await getStates();
       setStateNames(res);
     };
+    const fetchData = async () => {
+      const res = await getMissingData();
+      console.log(res)
+      setDataRes(res);
+    }
+    fetchData();
     fetchStates();
     setLoading(false)
   }, []);
-  console.log(stateNames)
+  // console.log(stateNames)
   
   const handleStateSubmit = (e) => {
     e.preventDefault();
@@ -39,16 +45,17 @@ export default function MapForm() {
           <option key={stateName.abrv}>{stateName.stateName}</option>
         ))}
       </select>
-      <select
-        value={stateNames.stateName}
+      {/* <select
+        value={dataRes.State}
         onChange={(e) => stateSelection.push(e.target.value)}
       >
-        {stateNames.map((stateName) => (
-          <option key={stateName.abrv}>{stateName.stateName}</option>
+        {dataRes.map((value) => (
+          <option key={value.State}>{value.State}</option>
         ))}
-      </select>
-      <button type='submit'>Test</button>
+      </select> */}
+      <button type='submit'>Submit</button>
       <button onClick={console.log(activeStates)}>Test 2</button>
+      <button onClick={console.log(dataRes)}>Test 3</button>
     </form>
     </>
   );
--- src/views/Data/Data.jsx
+++ src/views/Data/Data.jsx
@@ -1,3 +1,12 @@
+import { useActiveStates } from "../../context/Profile/StateCtx";
+
 export default function Data() {
-  return <h1> Data Page </h1>;
-}
+  const {activeStates } = useActiveStates();
+  return (
+    <>
+      <h1> Data Page </h1>
+      <h2>{activeStates[0]}</h2>
+      <h2>{activeStates[1]}</h2>
+    </>
+  );
+};
