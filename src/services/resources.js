27896be5e709bd9b5966ce385d4cd4338cc3c909
maddenlockin <maddenlockin@gmail.com>
2022-01-21 14:52:08 -0800

    push

--- src/services/resources.js
+++ src/services/resources.js
@@ -3,6 +3,7 @@ const URL = process.env.REACT_APP_DATABASE_URL;
 
 export async function getResources() {
   const url = `https://git.heroku.com/nameless-cove-57126.git`;
+
   const res = await fetch(url, {
     method: 'GET',
     headers: {
