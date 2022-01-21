10fcfffeceac66bf8a3a898e0f67363146c11219
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2022-01-20 16:42:36 -0800

    terrible css

--- src/hooks/UseLogin.jsx
+++ src/hooks/UseLogin.jsx
@@ -1,47 +1,54 @@
-import GoogleLogin, { useGoogleLogin } from "react-google-login";
-import { useProfile } from '../context/Profile/ProfileCtx'
-import { useNavigate } from "react-router-dom";
-import { checkEmails, findAuthEmail, signUp } from "../services/auth";
-import { createProfile, getProfileEmail, getProfiles } from "../services/profile";
+import GoogleLogin, { useGoogleLogin } from 'react-google-login';
+import { useProfile } from '../context/Profile/ProfileCtx';
+import { useNavigate } from 'react-router-dom';
+import { checkEmails, findAuthEmail, signUp } from '../services/auth';
+import {
+  createProfile,
+  getProfileEmail,
+  getProfiles,
+} from '../services/profile';
 
-
-const secret = process.env.REACT_APP_GOOGLE_CLIENT_ID
+const secret = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 const clientId = `${secret}.apps.googleusercontent.com`;
-const superSecret = process.env.REACT_APP_SECRET
-
+const superSecret = process.env.REACT_APP_SECRET;
 
 function LoginHooks() {
-  const {profile, setProfile, setAuthorized} = useProfile()
+  const { profile, setProfile, setAuthorized } = useProfile();
   const navigate = useNavigate();
 
-
   const onSuccess = async (res) => {
-    //--email from google response--// 
+    //--email from google response--//
     const email = res.profileObj.email;
-    await setProfile({email});
+    await setProfile({ email });
     console.log(res.profileObj);
     //Recieved access token from google--//
     //--Set app ctx authorized to true--//
-    setAuthorized(true)
+    setAuthorized(true);
     //--Check matching email in auth table--//
     const beRes = await findAuthEmail(res.profileObj.email);
     console.log(beRes);
     //--if email exists run log in route--//
-    if(beRes.email) {
-      const authorizeEmail = signIn({email: profile.email, password: `${superSecret}`});
+    if (beRes.email) {
+      const authorizeEmail = signIn({
+        email: profile.email,
+        password: `${superSecret}`,
+      });
       // const authorizeEmail = signIn({email: beRes.email, password: `${superSecret}`});
       console.log('SUCCESS SIGNING IN', authorizeEmail);
     } else {
       //--else run create route--//
-      const registerEmail = await signUp({email: res.profileObj.email, password: `${superSecret}`});
+      const registerEmail = await signUp({
+        email: res.profileObj.email,
+        password: `${superSecret}`,
+      });
       console.log('GOOGLE SUCEEDED ADDING ACT', registerEmail);
       navigate('/create');
     }
-  }
+  };
 
   const onFailure = (res) => {
-    console.log('Login failed:', res)
-  }
+    console.log('Login failed:', res);
+  };
 
   const { signIn } = useGoogleLogin({
     onSuccess,
@@ -51,8 +58,10 @@ function LoginHooks() {
   });
 
   return (
-    <button onClick={signIn}>Auth w/ google</button>
-  )
+    <button className="authButton" onClick={signIn}>
+      Auth w/ google
+    </button>
+  );
 }
 
 export default LoginHooks;
--- src/views/Layout/Header.jsx
+++ src/views/Layout/Header.jsx
@@ -2,22 +2,31 @@ import { NavLink } from 'react-router-dom';
 import { useProfile } from '../../context/Profile/ProfileCtx';
 import LoginHooks from '../../hooks/UseLogin';
 import LogoutHooks from '../../hooks/UseLogout';
+import css from './Header.css';
 
 export default function Header() {
   const { profile } = useProfile();
   return (
     <>
-      <h1>Outlier</h1>
-      {/* <h3>{profile.email}</h3> */}
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/instructions"> Instructions</NavLink>
-      <NavLink to="/map"> Map</NavLink>
-      <NavLink to="/data"> Data</NavLink>
-      <NavLink to="/about"> About</NavLink>
-      <NavLink to="/resources"> Resources</NavLink>
-      <NavLink to="/profile"> Profile</NavLink>
-      <LoginHooks />
-      <LogoutHooks />
+      <section className={css.HeaderSection}>
+        <div>
+          <h1>Outlier</h1>
+        </div>
+      </section>
+      <section className={css.button}>
+        <LoginHooks />
+        <LogoutHooks />
+      </section>
+      <section className={css.LinkSection}>
+        {/* <h3>{profile.email}</h3> */}
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/instructions"> Instructions</NavLink>
+        <NavLink to="/map"> Map</NavLink>
+        <NavLink to="/data"> Data</NavLink>
+        <NavLink to="/resources"> Resources</NavLink>
+        <NavLink to="/about"> About</NavLink>
+        <NavLink to="/profile"> Profile</NavLink>
+      </section>
     </>
   );
 }
