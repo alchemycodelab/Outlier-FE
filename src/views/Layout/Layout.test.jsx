48aff26782384537b5cafe38623595546fbdf320
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2022-01-20 18:59:14 -0800

    2nd test tweak but STILL PASSING

--- src/views/Layout/Layout.test.jsx
+++ src/views/Layout/Layout.test.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import Layout from './Layout';
 import { ProfileProvider } from '../../context/Profile/ProfileCtx';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Footer from './Footer';
-import Header from './Header';
 import { render } from '@testing-library/react';
 
 it('should render the layout', () => {
   const { container } = render(
     <Router>
       <ProfileProvider>
-        <Header />
-        <Footer />
+        <Layout />
       </ProfileProvider>
     </Router>
   );
